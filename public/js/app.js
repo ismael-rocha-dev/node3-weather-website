67da8d654a400f39be2792adb05bf40de463f1b5
Ismael Lima Rocha <ismaellimarocha23@gmail.com>
2021-05-13 21:18:39 -0300

    added a % sign

--- public/js/app.js
+++ public/js/app.js
@@ -13,26 +13,28 @@ const p2 = document.querySelector('#p2')
 
 
 
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
 
     const place = encodeURI(weatherInput.value)
 
-    fetch('/weather?adress=' + place).then((response)=>{
-    response.json().then((data)=>{
+    fetch('/weather?adress=' + place).then((response) => {
+        response.json().then((data) => {
 
-        if(data.error){
-            p1.textContent = data.error
-        } else{
-            p1.textContent = data.location
-           const day_night = data.forecast.is_day=="yes"?"It is day":" It is night"
-            p2.textContent = data.forecast.weather_descriptions + ". The temperature is " + data.forecast.temperature +"°C . It feels like " + data.forecast.feelslike + "°C. " + day_night + ". The humidity is: " + data.forecast.humidity
-            console.log(data.forecast) 
-        }   
-        
+            if (data.error) {
+                p1.textContent = data.error
+            } else {
+                p1.textContent = data.location
+                const day_night = data.forecast.is_day == "yes" ? "It is day" : " It is night"
+                p2.textContent = data.forecast.weather_descriptions + ". The temperature is " +
+                                 data.forecast.temperature + "°C . It feels like " + data.forecast.feelslike + "°C. "
+                                 + day_night + ". The humidity is: " + data.forecast.humidity + "%"
+                console.log(data.forecast)
+            }
+
+        })
     })
-})
 
 })
 
